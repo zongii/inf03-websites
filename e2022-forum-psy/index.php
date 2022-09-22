9c566b7a9e0a90c369a9f281079d7fdaecbd315e
Zongi <67127030+zongii@users.noreply.github.com>
2022-09-23 01:37:44 +0200

    added 2022-forum-psy

---
+++ e2022-forum-psy/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl.css">
+    <title>Forum o psach</title>
+</head>
+<body>
+    <div class="banner">
+        <h1>Forum Miłośnikmów psów</h1>
+    </div>
+    <div class="container">
+        <div class="leftBlock">
+            <img id="avatar" src="#" alt="avatar">
+            
+        </div>
+
+    </div>
+    <div class="footer">
+
+    </div>
+</body>
+</html>
